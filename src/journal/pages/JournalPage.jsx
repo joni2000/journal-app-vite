617f110c3dd670cb66a5b0b13869c02384f92d60
Jonathan Ibarrola <ibarrolajonathan@gmail.com>
2022-06-23 21:50:41 -0300

    agregando un boton flotante para añadir notas

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views";
 
@@ -9,8 +11,24 @@ export const JournalPage = () => {
 
       {/* <Typography variant='h1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid eos sunt hic pariatur, ex voluptas saepe reprehenderit quis ea aliquam placeat iste assumenda corrupti incidunt omnis blanditiis. Veniam, perferendis alias?</Typography> */}
 
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ':hover': { backgroundColor: "error.main", opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+
+          <AddOutlined sx={{ fontSize: 30 }}/>
+
+      </IconButton>
       
     </JournalLayout>
   )
